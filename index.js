60cab1c49a00ff7d0577753cc217c2ee92a0d35c
ivailo361 <ivailo361@gmail.com>
2021-01-13 22:52:12 -0800

    https added

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const env = process.env.NODE_ENV || 'development';
 const config = require('./config/config')[env];
 
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const privateKey  = fs.readFileSync(__dirname + '/sslcert/key.pem', 'utf8');
+const certificate = fs.readFileSync(__dirname + '/sslcert/cert.pem', 'utf8');
+const credentials = {key: privateKey, cert: certificate};
+
 const express = require('express');
 const app = express()
 const cors = require('cors');
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
 
 const connectDB = require('./config/connectDB')
 const stockRouter = require('./src/stock/routerStock');
@@ -34,5 +43,6 @@ connectDB().then(() => {
         console.log('*'.repeat(90))
     });
 
-    app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
+    httpServer.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
+    httpsServer.listen(4444, console.log(`Listening on port ${4444}! Now its up to you...`));
 })
